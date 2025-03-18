0458d2ac6b3ae568189822d8e6dd5dc8ff5ad204
VihaanGayakwad <businessemail1032@gmail.com>
2025-03-18 16:23:55 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ export const accounts = [
     {
         username: "FREE",
         password: "FREE",
-        hasMembership: true
+        hasMembership: false
     },
     {
         username: "Nikeraider",
